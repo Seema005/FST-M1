600f5011e747fa7969c3dde0047293b228fc44ae
Seema005 <143474433+Seema005@users.noreply.github.com>
2023-09-17 10:46:05 +0530

    Add files via upload

---
+++ Java/Activities/sample1.java
@@ -0,0 +1,8 @@
+package Example;
+
+public class sample1 {
+    public static void main(String[] args) {
+        String text=("Hello world!!");
+        System.out.println(text.toUpperCase());
+    }
+}
